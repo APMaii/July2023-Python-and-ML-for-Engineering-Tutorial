36e13de63e60ad16d059865b7856ceec79abd02b
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2024-08-30 22:20:02 +0330

    Create Session2_Course23.py

---
+++ Session2_Course23.py
@@ -0,0 +1,317 @@
+
+'''
+
+جلسه دوم 
+
+Ali Pilehvar Meibody
+gamlab.aut.ac.ir
+alipilehvar1999@gmail.com
+ai.course22.alipilehvar@gmail.com
+
+'''
+
+
+# zamani k haashtag bezanim ya qoute '' , code oon ro be onvane
+#comment dar nazar migire va nemikhone
+
+#python mesle yek motarjem az bala be paeen az chap be rast ejra mikone
+
+'''
+Vocabe python do bakhshe :
+    1- keywrod ( کلیدواژه )
+    2- built in function ( تابع داخلی)
+
+
+step1 : insert data
+step2: code ( calculate )
+step3: output 
+
+
+
+insert data : 1-OPEN --> tabeye dakheli 2-myself 3-download
+
+variable== case ( zarfe)
+
+a =! A
+
+
+
+structurez:
+    
+name = value
+
+
+variable : 1-number ( 1.1.int 1.2.float  )  2.string(reshte)
+'''
+
+b=5
+#type , نوع آن چیست
+# اعداد = int, float , complex
+
+
+#hamashoon int hastan
+b=6
+c=8
+c=8327982379176
+d=0
+
+
+#float = اعشاری
+adad=1.4
+
+
+#reshte moarefi krdn
+#typesh str hast
+name='ali'
+
+#96239872336
+idd=962871661872
+
+
+#esm famili
+
+fname='ali'
+lname='pilehvar'
+#ukuhkhkhkjhkhkhkh
+varzesh='football'
+football='real'
+
+
+name=''
+
+
+#fargh daran !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! *****
+name
+'name'
+
+a=56
+a
+56
+
+
+#kholase
+#dar ghesmate aval ma oomadim vared konim data ra
+#vared krdn se rah dare
+#1- az file bardashtan 2-khodemon tarif konim 3-az site download konid
+#vaghty bekhahim khodemon tarif konim
+#kolan injorie ---> name=value
+#value or variable chand ta type drn
+#number --> int ( sahih) , float ( ashari) , complex (1i , 1j)
+#text---> str ( yani string reshte) *** havasemon bashe quot bznim ' '
+
+
+#=======================================================================
+#assign = meghdardahi
+#name = value
+
+#name = mitone harf bashe adad bashe bozrog kochik 
+a_
+a_____
+a__b
+a76238565
+2323sd
+
+#*** agar fght adad
+233268 #!!!
+
+Ali #barabar nist ba
+ali
+
+#avalin mesal
+x1=30
+x2=40
+x3=50
+x4=2
+
+
+
+#-----operator [ محاسبه گر]
+#Arithmetic حسابی
+# جمع +
+#تفریق -
+#ضرب *
+#تقسم /
+#توان **
+# باقی مانده %
+
+
+20+30 #dastoor ejras mishe zakhire nmishe *****
+j=20+30
+
+
+
+x1+x2 #mohasebe mikone ( namayesh mide) zakhire nmikonb
+
+x5=x1+x2 # mohasebe mikone o zakhire mikone
+
+
+x1-x2
+x1*x2
+x1/x2
+
+
+#---------مقایسه comparison
+
+y1=10
+y2=20
+y3=10
+#مساوی ==
+#> < ببزرگتر و کوچکتر
+# >= <=
+# !=
+
+y1==y2
+#false
+y1==y3
+
+
+#*** farghe == ba =
+y1=10
+y2=20
+y1==y2 #moghayese comparison
+y1=y2 #meghdardehi assign
+y1=4398489
+#avale khat harchi mikhone khali mikoen
+
+y1>y2
+
+
+
+
+#logical منطقی
+#dota motegheyr ro dar nazar begirid
+# har dotashon 
+and
+
+#ya ykodom
+or
+
+
+
+#identity  تشخیص
+
+is
+
+y1 is 10
+
+
+is not
+
+#memebrship عضویت
+
+y in #felan lisdt
+
+
+
+
+#in mohit bvaraye neveshtane cod hast
+#assign konid meghdardehi konid
+#operator ( + _ ) estefade konid
+
+
+a=40
+
+b=30
+
+#a+b
+
+c=a+b
+
+
+# do raveshe digar darim baraye vorodi kgrftn
+#yekish ine ke ma yek file i darim 
+
+#az koja file ro brdrim?
+#chijori varede spyderr konim
+#chijori zakhirash konim?
+
+#c:/ssssaaaa
+
+#** harmoghe tabe didid , donbvale parantezesh bashid
+
+open('c:/usderr/computer/esmeshoma/foldedesiredssssaaaa')
+
+
+
+z=open('c:/usderr/computer/esmeshoma/foldedesiredssssaaaa')
+#mire z ro msiaze choon nist, bad narenji ro mibine mige tabe hast
+#tabe parantez dre , dakhele parantez addrsss has mire onaj file ro vrdmidre
+#mirize dakhele zarfe z va zakhirash mikone
+
+a=open('c:/usderr/computer/esmeshoma/foldedesiredssssaaaa')
+#fght y frgh , mire migrde mibine a hast , oono khali mikone va .....
+
+
+
+#sakhtar structur e yek code:
+#==========vorodi   input===========
+#==========body badane==============
+#==========khoroji output===========
+
+#noye aval --> 
+a=50
+#az bala mikhone , a ro migrde , khali mikone toosh 50 mirize
+#aya namayesh mide dar console ?? kheyr
+
+#p mohem hast masalan
+#1000 ta variable drid ama p ro mikhayd
+#10000 variable shoma oon bala samte rast ghesmate variable ex[plorer save mishe
+#ama hich ja namayesh dade nemishe
+
+#yek tabe
+#behesh khorojiiii
+#tabe ast --> narenji mishe --> parantez dre
+
+print(a)
+#dakhlesh oon chizi ke man mikam namayesh dade beshe
+#*** oon moteghayer
+
+print(b)
+
+print(salam)
+#fght variable bzarid dakhelesh
+
+
+#faghat name
+
+#chantaee? bale mishe ba ,
+print(a,b,c)
+
+name='ali'
+lname='pilehvar'
+
+print(name)
+
+print(name,lname)
+
+#shoma mitoonid adad ham ezafe konid hamrahe str
+print(name,a)
+
+#joda az esme moteghayer, mitonid khodeton value bzarid
+print(433234)
+
+
+
+#hala ma chanta value , yedoone adad yedoone ham str
+#str ro bkhayn bnvisim
+#bayad qoute
+''
+
+print('be payane jalase residim')
+
+print(be payane jalase residim) #errorrrrrrrrrrrrrrrrrr
+
+
+
+#==================kholase=========
+
+#یک فردی انتخاب کنید , داخل کد , اسمشو تعریف کنید 
+#, فامیلیشو تعریف کنید , سنش رو تعریف کنید , 
+#محل زندگیشو تعریف کنید 
+
+#خروجی اصلی : 
+    #اون پایین سمت راست  ( داخل کنسول ) نوشته بشه
+
+    
+    #سلام من ... هستم و سنم .... هست و .... زندگی میکنم
+
