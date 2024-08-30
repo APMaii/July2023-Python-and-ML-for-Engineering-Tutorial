b366fda2a35749eb5df7e9fd01d8bddefeb5b987
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2024-08-30 22:22:21 +0330

    Create Session8_Course2023.py

---
+++ Session8_Course2023.py
@@ -0,0 +1,651 @@
+"""
+Life is God's novel. Let him write it
+
+Created on Sun Aug 13 14:33:54 2023
+@author: Ali Pilehvar Meibody
+
+GAMLAB.AUT.AC.IR
+
+"""
+'''
+python--> 100000 library ___> ye applicationdre
+
+#sakhtane ketabkhone asoone? shoma mitonid hamon madule k neveshti
+#madul? hamoon script hamin safe hamin safe chat 
+#bzarid begid ketabkhoans
+#ama khob inkari nmdre
+#library 
+#*****
+
+
+
+#Gamlab library --> Pip poshtibani kone
+
+version=0.0
+
+'''
+
+#=======moshkelate bache haaaa========
+
+#tabe chie ?
+
+#ma yek no raveshe cod nvisi dahstim
+
+#vorodi migrftim
+open() #data migrft
+input() # az karbar migrf
+
+#haminja calculation va mohasebat anjam midad
+
+#ya save mirkd dar yek variabl
+#ya print
+print()
+
+
+
+#----yek raveseh jadid soragh drm --> function
+#yanmi kolesho bzar too ye capsulllllllll
+
+def name(input1,input2,input3):
+    #calculation
+    #calculation2
+    #it comprises of if, for , while
+    #....
+    x=input1
+    #khoorjii---> 1)fght print kone 2)return kone
+    return x
+   
+    
+#tarigheye estefade
+#sedash bznim va azash estefade konim
+
+
+
+x1=input('x1 ra vared konid')
+def aval(x1):
+    x2=x1*2
+    return x2
+
+#input chist?
+
+#asan input niazi nis pas chijori estefade konim?
+
+aval(x1)
+
+#***** pas ye tabe kafie niazi b input nis
+
+
+#import bayad biron abshe
+
+#import chi bod? vaghty 
+#ghabl az estefade az har tabee az har library bayad onoi import krd
+#koja?
+#avale code
+
+#sakhtare tabe
+def jam(x1,x2):
+    '''
+    
+    this function add two numebrs
+    Parameters
+    ----------
+    x1 : int
+        first number.
+    x2 : int
+        second number.
+
+    Returns
+    -------
+    None.
+
+    '''
+    c=x1+x2
+    
+#tabe do bakhshe 1-sakhtesh 2-seda zadanesh
+#ta inja sakhtim
+#sedash bznim
+jam(4,5) #hichi ndad
+#moshekl kojas?
+#ma tabe tarif krdim
+#vorodi tarif krdim
+#body ( calculation) tarif krdim
+#ama khoroji ndrim ke
+
+#3 no khoroji has---------------
+#1- yekish hamine ke asan khoroji nde !!!!
+#2-print kone javab ro
+#3-return kone
+
+#print yani vghty seda miznim tabe ro tabe fght yel
+#dastoore k too consule print mikone natije ro
+#marsooon nisssssss!!!!!!!
+
+#3-return--> bnargardonnnee-->koaj?
+#hamonja k sedash xzdim
+#pas hala in tabe tahvil dhndas na dastopor
+def jam(x1,x2):
+    c=x1+x2
+    return c
+
+a1=jam(4,5)
+
+#yek noktye dg 
+#olaviate operator ha
+
+c=a+b*5
+#** > * > / > + > -
+c=(a+b)*5
+
+
+#=====================================
+#tabe ro kamel fhmidim
+#chijopri barname benvisim????**********************
+
+#root ro hesab kone --> rishe hesab koni
+
+#1- saol ro tahlil kon----> answers are in the problem
+
+#soalo chijori tahlil konim
+#yek tabe ghrare benvisim--> pas ye box drim
+#ye seri vorodi migire yeseri khorojimide
+
+#soale aval--> chi vorodi bgirim
+#chi khoroji bdim
+
+4*x**2+8*x+8=0
+#in mani ndre too python
+#fght mitonim assign konim
+#masalan:
+x=40
+r=30
+t=40
+x=r/t
+
+#moadele ro chiori bdim?
+
+#aval bnvid
+
+def tabeyema(vorodi):
+    #calculation
+    return khoroji
+
+#aval bgim borodimon chie?
+#ye moadelas
+#tabdilesh konim
+#a*x**2+b*x**1+c=0
+
+#in ro nmitonim vared konim cchio vared konim/
+#begim zarayeb ro b trtib ---> description
+def root(a,b,c):
+    #calculation
+    return r
+
+
+root()
+#khoroji chi bgirim yani=== vaghty karbar sedash mizne tabe ro chi biad jash return she
+#root --> r --> rishe
+
+
+#pas tya alan
+#shod yek jabe ke zarayeb migire
+#va bema r ro mdie
+
+
+#step2=>> ensan chijori hal mikone bad zabone human language ---> machien language
+import math
+#delta=b2-4ac aval hesab mikonim
+#bad migim age ---(esme age omd yani if mikhad)
+def root(a,b,c):
+    #step hae k ye ensan anjam mide
+    delta=b**2-4*a*c #aval delta hesab mikonim
+    
+    #age delta mosbat bood
+    #age delta manfi bood
+    #ag delta sef bod
+    
+    if delta>0:
+        r1=(-b+math.sqrt(delta))/(2*a)
+        r2=(-b-math.sqrt(delta))/(2*a)
+        
+        return r1,r2
+
+    if delta==0:
+        r=-b/(2*a)
+        return r
+    
+    if delta<0:
+        
+        print('there is no answer')
+        #return None
+    #ye karee dg
+    
+#4*x**2 + 6*x + 8=0
+
+r1=root(4,6,8) #None type--> yani ndre
+
+#khodesh---> omd vase ma None type dad
+
+#soale dg --> khob in ke daraje 2 bood daraj ehaye balatar chi?
+
+#-----besh fek konid--> yek tabe besazid--> moshatgh begire
+
+#masalan in bood
+##4*x**2 + 8=0
+#root(4,8)
+#dota kar mitonim konim
+#too tozihat bshs bgim ag ham nazadsi agha 0 bzar
+
+#yni bnvis (4,0,8)
+
+#yek kare talaeee --> deafauelt ssakahtan
+def root(a,b=0,c=0):
+
+    #yni ag taraf yadsh rft zarib ro bzare yani 0 bode
+    
+    
+#chra a ro nzshtim? choon tabe drje do hasttttt
+#hata miutonbim yek chizi bzarim,
+if a=0:
+    print('please give me degree 2 quation')
+    
+
+#kari k too ketab khone ha mikonan
+
+
+#---contribution------
+#--> mostagh bgire/ antegral bgire
+#rishe hal kone--> masalanm newton raphson va vav a
+
+
+
+
+
+#-------------KETABKHOONE-----------------
+
+#soragheketabkhoene ha mirim
+import math
+import cmath
+import datetime
+import statistics
+import random
+
+#----
+import numpy
+
+  
+#tarigheye  import
+
+import esme keteabkhone
+import esme ketabkhone as mokhafaf
+#yeja kh ketabkhone bozoge
+from esme ketabkhone import felann tabe
+
+#3 tarigheye importe
+
+import math
+#kolan kole ketabkhone ha 3 ta chizi dre
+#1-constant adade sabe
+#2-tabe ha
+#3-class( toosh por az tabe has)
+
+#chijori dastresi dsshte bashim?
+#noghte bznim mire tooye math
+#tab bznim miare auto
+
+math.e #Out[21]: 2.718281828459045
+
+#ehtemalan yek formol drim
+
+a=b*(math.e)*c
+
+#tabe chijori?
+#mikhaym 
+
+a=30
+a_cos=math.cos(a)
+
+#ham mitoni search bzni ham mitoni dot tab
+
+#cmath yekam pishrfte tare ama shabihe mathe 
+
+#pas harja too formul niaz bod 
+#az math va cmath estefade mikonim
+
+#yani chi??
+#ykeseri afrad nshasan shoro krdn ye tabe shabihe zir nvsht
+
+def sin(number):
+    #omdn tabeye mowj neveshtan
+    #mohasebat ro krdn
+    #4,5 khat
+    return sin
+
+#hala vaghty ma minvisim math
+#miad mire too ketabkhone
+#miznim math dot
+#mire tooye functionaaaa
+#esme tabe ro miznim
+math.sin() #in dar asl maro vasl mikone b in tabeye bala
+
+
+#-------------------
+import datetime
+
+x=datetime.datetime.now()
+print(x) #2023-08-13 18:19:55.582133
+
+#------------------
+#ketabkhoneye mohemi k alan mirim soraghesh
+import random
+
+#adade random chie???
+#dota tarifd--> yeki k kamelan tasadofi
+#yeki yani adade badi ba adade ghabli yejoor entekhab nashe
+
+
+#computer az tarigfeh dovomie misaze
+#tozie tasadof --> random distribution
+
+#yani baze ee k bhsh midim ehtemale entekhabe hrkodom cheghadre
+
+#avalin tabe az in madule
+random.random() #Out[26]: 0.22224276396589038
+
+
+#mige man miam az 0 ta 1 ye adade random bht midcam
+
+
+
+a=random.random()
+print(a)
+#0.7928316201914613
+#0.5155225901695972
+
+
+#in omd az 0 ta 1 bma adad dad
+#ag bkhaymr angesho khodmon entekhab konim chiu?
+
+a=random.randint(5,10)
+print(a)
+
+#yanii ye adade sahih beyne 5,6,7,8,9,
+
+a=random.randrange(5,10)
+print(a)
+#yechizi b esme step dre
+
+a=random.randrange(5,10,2)
+print(a)
+
+
+#do noo distributipon drim
+#avghty k mikhayma dade ashari entekhab kone
+
+
+#1-normal or gussian dstribution= yani vasatie ehtem,ale bishtri dre entekhab she
+
+a=random.gauss()
+
+#2-unifrom yani hamashon ye ehtemale netekhabe yeksan drn
+a=random.Random.unifrom()
+
+
+#AttributeError: type object 'Random' has no attribute 'unifrom'
+
+
+
+
+#===========================================
+#ina ketabkhone haye sade bodn randomo , 
+#math , .....
+
+
+import statistics
+#baraye amar hast
+
+#mean() miangin
+#median()  #adade vasat
+
+#median_high() #adade vasato begir beyne ono bishtrrin adade vasate
+
+#median_low()
+
+#mode()  #
+
+#variance()
+#pvariance()
+
+#pstdev()
+#stdev() enheraf az meyar
+
+
+statistics.mean()
+
+#iterable migire --> yani list migire 
+#iterable--> harchi ke lemeent toosh dasht
+
+a=[1, 3, 5, 7, 9, 11, 13]
+
+print(statistics.mean(a)) #7
+
+
+#biaym besazim bbinim chijori sakhtan
+
+def gamlab_mean(mylist):
+
+    numbers=len(mylist)
+    summ=0
+    for i in mylist:
+        summ=summ+i
+    mean=summ/numbers
+    return int(mean)
+
+print(gamlab_mean(a)) #7
+print(statistics.mean(a)) #7
+
+
+
+
+a=[1, 3, 5, 7, 9, 11, 13]
+print(statistics.median(a)) #7
+
+
+a=[1, 3, 5, 7,8, 9, 11, 13]
+print(statistics.median(a)) #7.5
+
+
+a=[1, 3, 5, 7, 9, 11, 13]
+
+print(statistics.median_high(a)) #7
+
+
+
+
+a=[1,1,2,4,5,7,7,7,8,9,11]
+print(statistics.mode(a)) #7
+
+
+a=[1,1,1,1,1,1,1,2,4,5,7,7,8,9,11]
+print(statistics.mode(a)) #1
+
+
+#harchi in adad bzoorgtr yani distribution pahn tar tare yani dade ha door tar az mianginan
+
+statistics.stdev(a) #Out[54]: 3.5456210417116734
+statistics.variance(a) #Out[55]: 12.571428571428571
+math.sqrt(12.571428571428571)
+
+#yani  variance = stdev**2
+
+#khob in dota vase ine bbinim cheghad dade ha paeralkandan
+
+#chra variance estefade mishe ?
+
+#parakandegio bishtr nshon mide
+
+#================================
+#avalie - basic --> yad grftiidddd--->
+
+
+
+#=================================
+#================================
+#=================================
+#=======PYTHON=====================
+
+#yadtone yechizi dashtim mesle list?
+#miomad adadraro list mirkd?
+
+#assign
+a=[3,4,5,6,2,4,2]
+
+
+#hame typi migrdf
+a=['ali','hossein']
+
+#hata
+a=[4,5,'ali',True]
+
+#andaze va sizesh
+len(a) #chanta elemeent tooshe
+
+#loop in the list
+for i in a:
+    print(i)
+    
+#access to erlement
+#bayad esme list ro bzarim ba beraketo index
+#** index az 0 shoro mishe
+newlist=['a','b','c','d']
+#in listemon emikham b sevomin element brsm
+
+#az 0 nbshmor
+newlist[0] #'a'
+
+thefirst_element_OF_newlist=newlist[0] 
+
+
+#slicing
+newlist[0:2] #0 o 1 o vrmidre 2naaaaa
+
+
+#az aval ta ...
+
+newlist[:3]
+#az ... ta akhar
+
+newlist[2:]
+
+#chanta ham method dasht
+#append
+newlist.append()
+#remove del va insert va ......
+#search bznid list built in function
+
+#limitation-------
+#1- soratesh kammeeee
+#2-1 bodi bood
+
+#***********
+#numpyyyy---> yek jaygozin baraye list ke
+#50x faster than list, vaa fght yek bodi ham nis
+#balke arraye hast matrix (2 bodi), 3 bodii-->
+#yuani fght ye radif list nis masan tradifo sootoonm
+
+
+
+#8 daghigh mese list yadesh begir
+
+
+#aval assign 
+
+#list
+a=[1,2,3,4]
+#or
+a=list((1,2,3,4)) #do parantez
+
+
+
+#numpy ham taghriban shabihe
+
+
+import numpy as np
+#yani harja nvshtm np yani numpy
+
+#baraye har ketbkhone bas esmesho seda zad
+
+a=np.array((1,2,3,4))
+
+#b haminsSADEGII
+
+#ma dg ta akhare python fght azin array ha estefade mikonim
+
+#choon saro karemon kolan ba matriso 
+#ya yekseri dade haye excelllee o 
+#sotonpo radif dre
+
+
+
+#1----numpyyy
+#2----matplotlib
+
+
+
+
+
+
+#jalase badish
+#pandas-------
+#voordo b machien elarning
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#--------------
+#--1------
+#yek bazi bsazid (tabe nista hamon input)
+
+#ke machien yek adad random entekhab kone va
+#ma hads bznim
+#ag doros bgim bordim ag na dobare tekrar she
+
+
+
+
+#-----2--- pishrafte tar:
+    #rahnamaede ham kone
+    
+#mese bazie tassss
+
+#---------------dar kenare in quizzz
+#function haye moheme reshte haro baraye man esrsal konand +
+#dose ta ide ddm
+#moshtagh, antegral , darovordsane rishe
+
+
