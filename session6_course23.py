637828ee2894689e943628ff5138a9c698d2cd6e
Ali Pilehvar Meibody <ali.pilehvarmeibody@studenti.polito.it>
2024-08-30 22:21:17 +0330

    Create session6_course23.py

---
+++ session6_course23.py
@@ -0,0 +1,879 @@
+"""
+IN THE NAME OF GOD
+
+Created on Sun Aug  6 16:53:22 2023
+
+@author: Ali Pilehvar Meibody
+"""
+
+#kole python fght buil in functionn ba keyword
+print()
+len()
+input()
+int()
+bool()
+range()
+type()
+#minvisim rngi msihe
+#yekseri dastoora bod
+if
+else
+elif
+for
+
+
+#yadeton bashe bazi chiza return drn
+
+4>2 #Out[11]: True
+#hich karei nkrd fght ejra krd
+a=4>2
+#miad a ro save mikone true
+bool()
+#kheyli moheme
+del(delet mikone)
+#frghe keyword ba buil in chie?
+#buil in narenji mishe , yek tabe hast yani parantez dre
+#yani vorodi migire khgorji mide
+#ama keyword banafsh mishe va fght ejra mikone
+del a
+True
+False
+in
+is
+name='ali'
+'a' in name  #Out[13]: True
+'ali' is name #Out[14]: True
+
+#mitonim saveshon konim?
+bb='a' in name 
+#b=True
+
+#--tarigheye hale yek problem (problem solving)
+#1- negah konid bbinid
+#chanta builtg in function niaz has
+print()
+input()
+len()
+range()
+#negah mikonid be keywordha
+if
+else:
+for
+in
+is
+#badesh type e oon chizi k saro kar drim
+int,bool,str
+#mirim too methodash
+#masaln ag str bood
+a.upper()
+a.replace()
+#ya list e
+
+#tooy strha
+#split,replac,lower
+Salam
+salam
+Salam
+text.lower() #salam
+
+#tooye list
+append()
+copy()
+
+#============================
+#dobar esoraghe if
+
+
+#python vaghty mikhone az bala be paeen az chap be rast
+#harchi narenji banafshe maid anjam mide chon midonatesh
+#amalgarharam mishnase + - 
+#dg hichi balad nis
+#hame chizo be cheshme esme variable mibine
+
+ali
+#nmidone ali chie
+#ino yek esme zarf
+ali=2312231
+
+#mohmtrin chizi k bahash bremikhore dastoorat hast
+if
+#vaghty if ro mibine shoro mikone b algorithm chidan vase khodesh
+
+
+#----sakhatre if structure------
+#enter k zadim fasle endakht
+#chghd fasele ? andaze ye tab
+#tab kenare Q
+#balaye caps
+#har tab 4 ta fasdelas
+
+
+if shart:
+    statement ( yani dastori kmikhaym ejra bshe)
+    1
+    2
+    3
+    4
+    5
+    6
+    
+#aval if ro mikhone hsrate rooberosh mirese
+#miad mibine sharte true has ya false
+
+#stoppppppp
+3>4 #Out[16]: False
+#shartemon ya true has ya false
+#karborde in va is
+
+name='ali'
+'a' in name #Out[17]: True
+#ma yek edea kardim miad mige true yta false
+
+'b' in name #Out[18]: False
+
+#nesbat b false bodn ya true bodn miad va if ro ejra mikone
+#ag true bood mire harchi bad az fasele ke besheh
+#badaneye if bashe ro ejra mikone
+
+#-------ghalat
+if a>4:
+print('ali')
+#migfe montazere bvadaneye if has
+
+if a>4:
+ print('ali')
+ #man montazere badaneye if hastam
+ #badaneye if chghdre ? mige andaze ye yek tab
+ 
+ 
+if a>4:
+    print('ok')
+    
+#ta koja ejra mikone ta harj ain fasele hefze
+#yani fasele ro hazf konim badane tamom mishe
+#be ebarati az if miaym biuron
+
+if a>4:
+    print('ok')
+    
+#che bood ch nabood che true bod ch false nbo
+#revale adi   
+print('no')
+    
+#hamin ghazie baraye if else ham has
+
+#lese biad zire if
+if a>4:
+    print('ok')
+else:
+    print('no')
+    
+#hrchi inja nvshte shode kari bn if o else ndre batraye hame ejra mikone
+
+
+
+#for chie???
+#hrchio goftm bzarid kenar
+#******************************************************
+#aval yek shomarande drm
+#be tedade oon miam yek halghe ro hey jera mikonm hey tekrar mikonm
+
+#structure
+for shomarande :
+    
+    
+#shomarande chie? hare esme zarfi k bshh bdim amas ma aksaran migim i
+
+name='alipilehvar'
+for item in name:
+for chiz in name:
+for x in name:
+for i in name:
+
+    #mige az aval bia oon chiz ro ( item, i , x,a)
+#yek bar mosavi grhar bde ba avalin caracter va ejra kon 
+#oon badaneye for ro 
+#dobare i ro caractere badi bzar ejra kon
+#ta akharrr
+name='alipilehvar'
+for x in name:
+    print(x)
+#a
+#l
+#i
+#p
+#i
+#l
+#e
+#h
+#v
+#a
+#r
+
+#be in migan loop
+#hala ma mikhaym adad bdim
+
+for x in [1,2,3,4,5]:
+    print(x)
+    
+#khob x item chiz hich frghi ndrn bia az I estefade konim hame
+
+for i in [1,2,3,4,5]:
+    print(i)
+
+#aya fght bayad inaro ejra konim?
+#na ma yek badaneye for drim 
+#hamoon tookhali
+#mitonim har dastori bdim
+
+for i in [1,2,3,4,5]:
+    print('*')
+    
+    #bazi oghat asan kari b i ndrim
+    #fght mikhaym begim n bar yekaro kon
+    
+    
+
+#for  2 ta applicaztion dre kardvbord:
+    
+#1- biad iter kon yua loop bzne tooye yek i9terable
+#yani too yek reshte (str) ya tooye yek list biad done done element haro check kone
+
+#2-biad be ye n bari yek kario kone
+for i in [1,2,3,4,5]:
+    print('*')
+#ag khastim bnvisim 1 ta 100 , brim bnvisim [1,2,3,4,5,54,,65,,,754,75]
+
+#python gof man range drm
+
+for i in range(0,n)
+#miad az 0 ta n {n bar} yek kario mikone ( dastoore ziro)
+
+
+
+for i in range(0,100):
+    print('sorry')
+    
+#range yek nokte dre range(start,end)
+#end ro hesab nmikone
+#az start mire ta end-1
+range(0,5) #0,1,2,3,4 
+range(0,10,2) #0,2,4,6,8,
+range(10,0) #ejra nmishe
+
+#bbin for az while omde
+
+#miad yek start mide b shomarande
+#miad hey anjam mide az start
+#yek end ham mzirim 
+#b end mirse miad biron az halghe
+i=1
+while i<10:
+    print('salam')
+    i=i+1
+    
+#for ham haminkaro mikone
+
+for i in range(0,10):
+    print('salam')
+    
+#balaee ba paeeni yekie
+
+for i in range(5,0):
+    print('salam')
+#miad az aval i ro mizre 5 mige eee rasngemon has 0,1,2,3,4 # 5 nis tosh
+
+for i in range(5,0,-1):
+    print('salam')
+
+
+for i in range(5,0,-1):
+    print(i)
+
+#5
+#4
+#3
+#2
+#1
+
+
+#$chanta keyword drim k dasr for bekar mire
+
+
+for i in range(0,5):
+    print(i)
+#0
+#1
+#2
+#3
+#4
+
+
+for i in range(0,5):
+    if i==3:
+        break
+    print(i)
+
+#0
+#1
+#2
+
+for i in range(0,5):
+    if i==3:
+        continue
+    print(i)
+    
+#0
+#1
+#2
+#4
+
+#*** while zamani b kar mire ke entehash ro nemidonim*aklbate len ham bekomak miad
+
+#   q1
+user_database=['u1','u2','u3']
+newuser=input('salam uisereto begoo:')
+user_database.append(newuser)
+
+#q2
+num1=int(input('your first number:'))
+num2=int(input('your second number:'))
+
+amalgar=input('amalgar?:')
+
+if amalgar=='+':
+    c=num1+num2
+    print(c)
+if amalgar=='-':
+    c=num1-num2
+    print(c)
+if amalgar=='*':
+    c=num1*num2
+    print(c)
+    
+
+#yek ravesh chanta if bznim
+#raveshe dovom if else
+if amalgar=='+':
+    c=num1+num2
+    print(c)
+elif amalgar=='-':
+    c=num1-num2
+    print(c)
+elif amalgar=='*':
+    c=num1*num2
+    print(c)
+else:
+    print('ERROR')
+
+
+weight=int(input('your height:')
+height=int(input('your height:')
+           
+BMI=wight/height**2
+
+#amalgar ha taghadom drn
+
+# ** > * > / > + > -
+
+a*b-4
+#aval a ro zarb dar b mikone bad kolesho menhaye 4
+
+#komake doostan: esdtefade az parantez
+j=0
+if amalgar=='+':
+    c=num1+num2
+    print(c)
+elif amalgar=='-':
+    c=num1-num2
+    print(c)
+elif amalgar=='*':
+    c=num1*num2
+    print(c)
+else:
+    j+j+1
+
+
+j=0
+while 0<j:
+    num1=int(input('your first number:'))
+    num2=int(input('your second number:'))
+
+    amalgar=input('amalgar?:')
+    if amalgar=='+':
+        c=num1+num2
+        print(c)
+    elif amalgar=='-':
+        c=num1-num2
+        print(c)
+    elif amalgar=='*':
+        c=num1*num2
+        print(c)
+    else:
+        print('try again')
+        j+1
+        
+    
+
+
+
+password=input('your poasswsoerd:')
+
+len_pass=len(password)
+
+print(len_pass*'*')
+
+
+#goftam ba for besazid
+password=input('your poasswsoerd:')
+for i in password:
+    print('*',end='')
+    
+    #-------
+password=input('your poasswsoerd:')
+len_pass=len(password)
+
+for i in range(0,len_pass):
+    print('*',end='')
+
+
+#------
+for i in range(0,len(input('your poasswsoerd:'))):
+    print('*',end='')
+    
+#--------
+password=input('your poasswsoerd:')
+newpas=''
+for i in password:
+    newpass=newpass+'*'
+    
+
+
+#---password jaaye alef * bzne
+password=input('your poasswsoerd:')
+password.replace('a','*')
+
+#masalan pasword='salam'
+#ba for chtor?
+password=input('your poasswsoerd:')
+for i in pasword:
+    if i=='a':
+        i='*'
+
+password=input('your poasswsoerd:')
+for i in range(0,len(password)):
+    if password[i]=='a':
+        password[i]='*'
+         
+        
+#nbalaeie yebar i ro mizre s mizare a mizre a mizare l mizare a
+#ama paeenie i ro mzire 1 2 3 4 5
+    
+#nbazi oghat mikhaymbrim tooye oon zarf
+#iter konim
+#loop bznim
+
+
+
+#-----------security checker------------
+password=input('your poasswsoerd:')
+
+#mikhaym brim toosh done done ro begardim 
+#bazresiii
+
+#for mikhad
+#for ba range ? na item
+#peymayesh
+#svreen
+#gharbalgari
+password='salam'
+for i in password:
+    print(i)
+
+#if bzarim
+for i in password:
+    print(i)
+
+#soal= bozorg bashe toosh adad ham bashe
+
+for i in password:
+    if i.isdigit()
+    
+a='salam'
+a.isdigit() #Out[36]: False
+
+a=23
+a.isdigit() #AttributeError: 'int' object has no attribute 'isdigit'
+
+
+a='3'
+a.isdigit() #Out[38]: True
+
+#true false b chdrd mikhore? baraye sharte if
+password='salam456'
+for i in password:
+    if i.isdigit():
+        print(i)
+        
+#4
+#5
+#6
+
+#behesh migan shjomarande
+#yechizi mese j tarif kon 0 esh kon
+
+
+j=0
+
+password='salam456'
+for i in password:
+    if i.isdigit():
+        j=j+1
+    
+    
+    
+    
+print(j) #3 # yani se ta adad intoee toye password
+
+#k behesh midim
+
+
+
+
+# j drim k drim
+
+#ina 0 bodan 
+#age sefr bmonam yani estefade nshode
+#yani ag j 0 bashe yani adad nis
+#ag k nabashe yani capital nis
+
+and
+or
+
+
+#and yani joftesh baham ejra she
+#or yani hata yekodom
+
+if j==0 and k==0:
+    print('your pasword is invalid')
+    
+password='salam5'
+  #print nmikoen chra ?
+  
+    
+   
+    
+   
+    
+if j==0 or k==0:
+    print('your pasword is invalid')
+    
+    
+
+
+a=5
+b=7
+
+#ham 1 ham b bozorgtr az 5 bshe
+
+if a>5 and b>5:
+    print('saklam')
+    
+#na ag yekodomam balatyar az 5 bod okeye
+
+if a>5 or b>5:
+    print('salam')
+    
+    
+    
+capital='ABCDEFGHIJKLMNMOPQRSTZXYUW'
+number='0123456789'
+
+has_capital=False
+has_number=False
+
+
+for i in password:
+    if i in capital:
+        has_capital=True
+        
+    if i in number:
+        has_number=True
+        
+        
+#do adad baraye tajZie darim yeki has number yki has capital
+
+#ag jofteshon true bashe yani ham bozorg dre ham adad toshe
+#in awlie
+#ag joftesh dalse bashe-> yani password zaefe
+#ag yekodom fght bashe--> baz b drdemon nmikhore
+
+
+if has_number==True and has_capital==True:
+    print('secure')
+
+#JOFT OKEYE
+
+if has_number and has_capital:
+    print('secure')
+    
+#agar or mizdim yekoidom ro midido migof secure e k injori nis\
+    
+#too soala 
+#havaseton bashe 
+#   BUG PEYDA KONID
+
+#MASAN YE IF bzarid onja kmige adad 3 raghami ag nabod 
+#ya ya ya
+
+
+#===========DEF============Function==============
+
+
+
+#yek tabe vorodi dre calculation khooroji(optional)
+    
+    
+#avalin ghesmta: tarife tabe
+
+#aval def bad esme tabe bad parantez toye parantez vorodi ha
+#esme vorodi
+#donoghte
+#yek tab o fasele
+
+def first(input1):
+
+#chanta vorodi
+
+def first(input1,input2):
+   
+    #har esmi
+    
+def first(a,b):
+    a+b
+    if
+    for
+    a.append()
+    
+#age khoroji ndshte bashe ?
+
+
+def jam(a,b):
+    c=a+b
+    
+#tabe ro run mikonim fght save mishe ejra nmishe
+#ta vaghty sedash nznim hich etefaghi nmiofte
+
+#seda zadan= fasrakhanie tabe
+
+
+jam# esme tabe
+
+#chie? tabe --> ()
+
+jam() #vaghty vorodi ndre
+
+#chon mesale ma vorodi dre, vorodi haro bayad tooye parantez bznim
+#seda zadan bad az tarife tabe bashed
+
+
+jam(4,5)
+
+#khoroji dad ? na
+#chika rkrd?
+#a ro gozasht 4 , b ro gozasht 5 
+#c ro hesab krd
+#haminnn
+
+#too variable ha ham chizi b onvane C vojod ndre
+
+#avalin 
+
+global
+local
+#har chizi dakhele tabe shoma tarif mikopnid birone tabe
+#vojopde khareji ndre
+
+#magar inke poshtesh bnvisid global
+
+#khoroji chikar konim?
+#2 noe
+#yeki print vase tamasha
+#yeki return baragrdone
+
+def jam(a,b):
+    c=a+b
+    print(c)
+
+
+jam(4,5) #9
+#c ro save nkrd chon harchi dakhele tabe local has na global
+#ag bekhaym brgrdone yani khorji dshte bashe chi?
+
+
+def jam(a,b):
+    c=a+b
+    return c
+
+#avalin nokte: tabe vaghty b return brese baghiaro dg nmikhone
+
+#2- return yani jayee k farakhani ya sedash zadim miad mide bma
+
+
+jam(4,5)
+#9
+
+
+#tabe ro fhmidim
+#ychizi class
+#vali bdonid class tashkil shode az yekseri tabe + yekseri adade sabet
+
+#===================
+
+#madule, ketabkhone chie, 
+#@ketabkhone : yani yek file i has ke koli tabe dre 100 ta tabe dre
+#shayad 100 ta adade sabet dre masalan pi 
+#va shmoma kafie sedash konid varedesh konid (import)
+
+#bejaye neveshtane 100 khat kod fght az tabe hash estefade konid
+
+#file new
+
+#yek madule neveshtim
+
+#import bekhaym konim
+
+import #esme ketabkhone
+
+
+import firstmadule.py
+
+#ehtemalan mige peydash nkrdm
+chikar konim?
+#bayad to path bashe
+
+
+import firstmadule
+
+#.py niaz nis
+
+#yek no  raveshe import hast
+
+#harja kari ba tabe haye dakhele in ketabkhone drim madule drim
+#esmesho seda bznim
+firstmadule.pi #Out[4]: 3.14
+#pi yek adade , tabe nis pas niazi bn parantez ndre
+
+firstmadule.jam(4,5) #Out[5]: 9
+
+#yek raveshe import bod
+#raveshe dovom mokhafafe
+
+import firstmadule as f
+#yaNI ESME MADULE ro bnvis as bzar jolosh mokhafaf
+#har mokhafafi
+
+#hey nanevuisiam firstmadule
+
+f.pi
+#pas mitonm dstresi dashte basham ba dad
+
+f.jam(23,50) #Out[9]: 73
+
+
+f.taghsim(4,8) #Out[11]: 0.5
+f.taghsim(5,0) #change the second
+
+
+
+
+#chjanta madule maroof drim
+
+math
+cmath
+random
+request
+
+
+#hyarkodom yek kari
+
+import math
+import math as m
+#age erro ndad yani daratesh
+#age error dad baayd brim install konim
+
+import cmath
+
+import random
+import request
+
+
+#yeseri ketabkhone harfo defau;lf dare python ama age nadasht chi?
+
+
+
+
+
+#mirim esmesho search miznim
+
+
+#numpy
+#matplotlib
+#pandas
+
+import numpy
+
+#no madule named 'numoy''
+
+
+#berid internet ino search konid
+
+#pip numpy
+
+#pip esme madule
+
+
+#searech konid va brid site e pipy
+#oon paeene esmesh
+#copy konid
+
+#ke aksaRAN INJORIE 
+#pip install numpy
+
+
+
+
+#copy
+
+#koja paste koniM?
+
+#anaconda omd chanta mohit sakht vae ma
+#avalish base(root) bood
+#brid environmente jadid bsazid
+
+#az styart brid roosh
+
+#hgar mohit mitone vase khodesh package bgire
+
+#ba chi ? powershell
+
+
+#dar strart search konim powershell ( esme mohit)
+
+#baz mishe
+
+#toosh copy paste ro anjam midim va enter
+
+
+
+
+#====================================
+
+
+def jam(a,b):
+    c=a+b
+    return c
+
+d=jam(1,2)
+
+
